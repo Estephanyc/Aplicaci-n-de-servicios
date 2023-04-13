169cbd9be83cfe3a2e062a516e912a89603535a6
Francisca <fr.perez@live.cl>
2023-04-12 21:48:39 -0400

    modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FinanzasComponent } from './page/finanzas/finanzas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FinanzasComponent
   ],
   imports: [
     BrowserModule
