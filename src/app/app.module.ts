cc4f1bc52e9cdc3a472791a4fccab829b9d3fbec
estephany <maitilydevidasi@gmail.com>
2023-04-12 22:53:43 -0400

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-=======
 import { MenuComponent } from './menu/menu.component';
-import { FinanzasComponent } from './page/finanzas/finanzas.component';
->>>>>>> cccd13438bac354a80635fef7da8cf0374d948ce
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LoginComponent
-=======
-    MenuComponent,
-    FinanzasComponent
->>>>>>> cccd13438bac354a80635fef7da8cf0374d948ce
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule, routing
