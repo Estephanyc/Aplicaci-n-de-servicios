e649c8ff9fbda04ae4581687bb56bd0d774f59d9
Francisca <fr.perez@live.cl>
2023-04-15 16:40:26 -0400

    menu

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,18 @@ import { LoginComponent } from "./login/login.component";
 import { AppComponent } from "./app.component";
 import { MenuComponent } from "./menu/menu.component";
 import { EliminarFactComponent } from "./page/facturacion/eliminar-fact/eliminar-fact.component";
+import { IngresarFactComponent } from "./page/facturacion/ingresar-fact/ingresar-fact.component";
 
 
 const appRoutes = [
-  {path : 'home', component : AppComponent},
+  {path : 'home2', component : AppComponent},
   { path: "login", component: LoginComponent},
-  { path: "menu", component: MenuComponent}, 
-  { path: "facturacion/eliminarfact" , component:EliminarFactComponent}
+  { path: "home", component: MenuComponent, children:[
+    { path: "facturacion/eliminarfact" , component:EliminarFactComponent},
+    { path: "facturacion/ingresarfact", component:IngresarFactComponent}
+    
+  ]}, 
+  
 ];
 
 
